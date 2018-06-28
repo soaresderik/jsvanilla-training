3acabc2954c0241d5ceae3df5ea54a1490a4d626
Derik Soares <codigoaberto.info@gmail.com>
2018-06-28 17:16:26 -0300

    construindo EasyHTTP para requests HTTP)

---
+++ easyhttp/main.js
@@ -0,0 +1,59 @@
+/*
+
+const http = new easyHTTP();
+
+
+
+http.get('https://jsonplaceholder.typicode.com/posts', function(err, res){
+    if(err){
+        console.log(err);
+    }else{
+        console.log(res);
+    }   
+});
+
+http.get('https://jsonplaceholder.typicode.com/posts/101', function(err, res){
+    if(err){
+        console.log(err);
+    }else{
+        console.log(res);
+    }   
+});
+
+
+
+const data = {
+    title: 'Custom Post',
+    body: 'This is a custom post'
+};
+
+
+
+http.post('https://jsonplaceholder.typicode.com/posts', data, function(err, post){
+    if(err){
+        console.log(err);
+    }else{
+        console.log(post);
+    }
+})
+
+http.put('https://jsonplaceholder.typicode.com/posts/1', data, function(err, post){
+    if(err){
+        console.log(err);
+    }else{
+        console.log(post);
+    }
+});
+
+
+
+http.delete('https://jsonplaceholder.typicode.com/posts/1', function(err, res){
+    if(err){
+        console.log(err);
+    }else{
+        console.log(res);
+    }
+});
+
+
+*/
