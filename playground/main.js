fc401ff83c50fb245a8c1de2895e10083f233ca0
Derik Soares <codigoaberto.info@gmail.com>
2018-06-28 18:47:49 -0300

    Utilizando fetch API 2

--- playground/main.js
+++ playground/main.js
@@ -4,16 +4,11 @@ document.getElementById('button3').addEventListener('click', getAPI);
 
 function getText(){
     fetch('text1.txt')
-    .then(function(res){
-        return res.text();
-    })
-    .then(function(data){
-        console.log(data);
+    .then((res) => res.text())
+    .then((data) => {
         document.getElementById('output').innerHTML = data;
     })
-    .catch(function(err){
-        console.log(err);
-    });
+    .catch((err) => console.log(err));
 }
 
 function getJson(){
@@ -21,18 +16,14 @@ function getJson(){
     .then(function(res){
         return res.json();
     })
-    .then(function(data){
-        console.log(data);
+    .then((data) => {
         let output = '';
         data.forEach(function(post){
             output += `<li>${post.title}</li>`
         });
-
         document.getElementById('output').innerHTML = output;
     })
-    .catch(function(err){
-        console.log(err);
-    });
+    .catch((err) => console.log(err));
 }
 
 function getAPI(){
@@ -40,16 +31,12 @@ function getAPI(){
     .then(function(res){
         return res.json();
     })
-    .then(function(data){
-        console.log(data);
+    .then((data) => {
         let output = '';
         data.forEach(function(user){
             output += `<li>${user.login}</li>`
         });
-
         document.getElementById('output').innerHTML = output;
     })
-    .catch(function(err){
-        console.log(err);
-    });
+    .catch((err) => console.log(err));
 }
