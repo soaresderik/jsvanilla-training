797df2f5e987a41395f56fa45fb44a4e8b536bf5
Derik Soares <codigoaberto.info@gmail.com>
2018-06-28 12:26:02 -0300

    Atualizando para ES6

--- booklist/main.js
+++ booklist/main.js
@@ -1,56 +1,55 @@
-function Book(title, author, isbn){
-    this.title = title;
-    this.author = author;
-    this.isbn = isbn;
-}
-
-function UI(){
-
-}
-
-UI.prototype.addBookToList = function(book){
-    const list = document.getElementById('book-list');
-
-    const row = document.createElement('tr');
-
-    row.innerHTML = `
-        <td>${book.title}</td>
-        <td>${book.author}</td>
-        <td>${book.isbn}</td>
-        <td><a href="#" class="delete">x</a></td>
-    `;
-
-    list.appendChild(row);
-
-}
-
-UI.prototype.deleteBook = function(target){
-    if(target.className === 'delete'){
-        target.parentElement.parentElement.remove();
+class Book {
+    constructor(title, author, isbn){
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn
     }
 }
 
-UI.prototype.clearFields = function(){
-    document.getElementById('title').value = '';
-    document.getElementById('author').value = '';
-    document.getElementById('isbn').value = '';
+class UI {
+    addBookToList(book){
+        const list = document.getElementById('book-list');
+
+        const row = document.createElement('tr');
+
+        row.innerHTML = `
+            <td>${book.title}</td>
+            <td>${book.author}</td>
+            <td>${book.isbn}</td>
+            <td><a href="#" class="delete">x</a></td>
+        `;
+
+        list.appendChild(row);
+    }
+
+    showAlert(msg, className){
+        const div = document.createElement('div');
+        const container = document.querySelector('.container');
+        const form = document.querySelector("#book-form");
+
+        div.className = `alert ${className}`;
+        div.appendChild(document.createTextNode(msg));
+
+        container.insertBefore(div, form);
+
+        setTimeout(function(){
+            document.querySelector('.alert').remove();
+        }, 3000);
+    }
+
+    deleteBook(target){
+        if(target.className === 'delete'){
+            target.parentElement.parentElement.remove();
+        }
+    }
+
+    clearFields(){
+        document.getElementById('title').value = '';
+        document.getElementById('author').value = '';
+        document.getElementById('isbn').value = '';
+    }
 }
 
-UI.prototype.showAlert = function(msg, className){
-    const div = document.createElement('div');
-    const container = document.querySelector('.container');
-    const form = document.querySelector("#book-form");
-
-    div.className = `alert ${className}`;
-    div.appendChild(document.createTextNode(msg));
-
-    container.insertBefore(div, form);
-
-    setTimeout(function(){
-        document.querySelector('.alert').remove();
-    }, 3000);
-
-}
 
 document.getElementById('book-form').addEventListener('submit', function(e){
     const title = document.getElementById('title').value,
