54064747e4dcf0e1c0d73b8e1bed025cc413d336
Derik Soares <codigoaberto.info@gmail.com>
2018-06-28 14:57:17 -0300

    XMLHttpResponse na prática (Get Customer(s))

--- playground/main.js
+++ playground/main.js
@@ -1,8 +1,30 @@
-document.getElementById('button').addEventListener('click', loadData);
+document.getElementById('button1').addEventListener('click', loadCustomer);
+document.getElementById('button2').addEventListener('click', loadCustomers);
 
-function loadData(){
+function loadCustomer(e){
+    const xhr = new XMLHttpRequest();
 
-    /* 
+    xhr.open('GET', 'customer.json', true);
+
+    xhr.onload = function(){
+        if(this.status === 200){
+            const customer = JSON.parse(this.responseText);
+            const output = `
+                <ul>
+                    <li>#${customer.id}</li>
+                    <li>Nome: ${customer.name}</li>
+                    <li>Empresa: ${customer.company}</li>
+                    <li>Nome: ${customer.phone}</li>
+                </ul>
+            `;
+
+            document.getElementById('customer').innerHTML = output;
+        }
+    }
+
+    xhr.send();
+
+     /* 
        ------ Status HTTP ------
        200: OK
        403: Forbidenn
@@ -15,28 +37,48 @@ function loadData(){
        4: process finished and response is ready
 
     */
+}
 
+
+function loadCustomers(e){
     const xhr = new XMLHttpRequest();
 
-    xhr.open('GET', 'data.txt', true);
+    xhr.open('GET', 'customers.json', true);
 
     xhr.onload = function(){
         if(this.status === 200){
-            document.getElementById('output').innerHTML = `<h1>
-                ${this.responseText}
-            </h1>`;
+            const customers = JSON.parse(this.responseText);
+            let output = '';
+
+            customers.forEach(customer => {
+                output += `
+                    <ul>
+                        <li>#${customer.id}</li>
+                        <li>Nome: ${customer.name}</li>
+                        <li>Empresa: ${customer.company}</li>
+                        <li>Nome: ${customer.phone}</li>
+                    </ul>
+                `;
+            });
+            
+
+            document.getElementById('customers').innerHTML = output;
         }
     }
 
-
-
-    // xhr.onreadystatechange = function(){
-    //     console.log('READYSTATE ', xhr.readyState);
-
-    //     if(this.status === 200 && this.readyState === 4){
-    //         console.log(this.responseText);
-    //     }
-    // }
-
     xhr.send();
+
+     /* 
+       ------ Status HTTP ------
+       200: OK
+       403: Forbidenn
+
+       ------ Ready State ------
+       0: request not initialized
+       1: server connection estabilished
+       2: request received
+       3: processing request
+       4: process finished and response is ready
+
+    */
 }
